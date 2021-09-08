047845bab9c4d2012e9b5a03c2057debe67286b0
Pedro Vilela <46492888+phvilela87@users.noreply.github.com>
2021-09-07 23:05:38 -0300

    Starting project implementation

---
+++ PlanoDeEstudos/PlanoDeEstudos/StudyManager.swift
@@ -0,0 +1,37 @@
+//
+//  StudyManager.swift
+//  PlanoDeEstudos
+//
+//  Created by Pedro Vilela on 07/09/21.
+//  Copyright © 2021 Eric Brito. All rights reserved.
+//
+
+import Foundation
+
+class StudyManager {
+    static let shared = StudyManager()
+    let ud = UserDefaults.standard
+    var studyPlans: [StudyPlan] = []
+    
+    private init() {
+        if let data = ud.data(forKey: "studyPlans"), let plans = try? JSONDecoder().decode([StudyPlan].self, from: data) {
+            self.studyPlans = plans
+        }
+    }
+    
+    func savePlans() {
+        if let data = try? JSONEncoder().encode(studyPlans) {
+            ud.setValue(data, forKey: "studyPlans")
+        }
+    }
+    
+    func addPlan(_ studyPlan: StudyPlan) {
+        studyPlans.append(studyPlan)
+        savePlans()
+    }
+    
+    func removePlan(at index: Int) {
+        studyPlans.remove(at: index)
+        savePlans()
+    }
+}
---
+++ PlanoDeEstudos/PlanoDeEstudos/StudyPlanViewController.swift
@@ -0,0 +1,32 @@
+//
+//  StudyViewController.swift
+//  PlanoDeEstudos
+//
+//  Created by Eric Brito
+//  Copyright © 2017 Eric Brito. All rights reserved.
+
+import UIKit
+
+class StudyPlanViewController: UIViewController {
+
+    @IBOutlet weak var tfCourse: UITextField!
+    @IBOutlet weak var tfSection: UITextField!
+    @IBOutlet weak var dpDate: UIDatePicker!
+    
+    let sm = StudyManager.shared
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        dpDate.minimumDate = Date()
+    }
+
+    @IBAction func schedule(_ sender: UIButton) {
+        let id = String(Date().timeIntervalSince1970)
+        let studyPlan = StudyPlan(course: tfCourse.text!, section: tfSection.text!, date: dpDate.date, done: false, id: id)
+        
+        sm.addPlan(studyPlan)
+        navigationController!.popViewController(animated: true)
+    }
+    
+}
